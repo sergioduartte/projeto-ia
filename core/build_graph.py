317a5f11f47ed7dd97a07ce5802ecc59a5b88d3d
LukasNasciment0 <lukasnascimento1>
2025-11-20 11:23:28 -0300

    DONE building-graph function

--- core/build_graph.py
+++ core/build_graph.py
@@ -7,12 +7,13 @@ Build a graph from a json
 import networkx as nx
 import json
 
-
+'''
+Abre o arquivo através do seu caminho e transforma o conteudo json em variaveis 
+Tudo vira um dicionario no formato -> 'edges': [['A', 'B', 5], ['B', 'C', 7]]
+O valor da chave é uma lista de listas de tamanho 3 sendo [node, terminal, weight]
+'''
 def build_graph(path):
 
-    # Abre o arquivo 'graph.json' (caminho do arquvio), transforma o conteudo json em variaveis 
-    # Tudo vira um dicionario no formato -> 'edges': [['A', 'B', 5], ['B', 'C', 7]]
-    # O valor da chave é uma lista de tuplas com 2 nós terminais e o peso da aresta que os conectam
     with open(path) as graph:
         data = json.load(graph)
 
@@ -23,14 +24,37 @@ def build_graph(path):
     
     return G
 
-# testing
+# testing ----------------------
 
-G = build_graph("data/testing_building.json")
+archive_path = "/Users/lukasnascimentos/Faculdade/IA/projeto1/projeto-ia/data/testing_building.json"
+G = build_graph(archive_path)
 
 print("Nodes: ", G.nodes())
 print("Edges: ", G.edges(data=True))
 
-assert G.nodes() == {['A', 'B', 'C', 'D', 'E', 'F']} 
-assert G.edges(data=True) == "[('A', 'B', 5), ('A', 'C', 3), ('B', 'C', 10), ('C', 'D', 1), ('E', 'F', 5)]"
+'''
+This is how the json looks like:
+{
+    "edges" : [
+            ["A", "B", 5],
+            ["A", "C", 3],
+            ["B", "C", 10],
+            ["C", "D", 1],
+            ["E", "F", 5]
+    ]
+}
+'''
+
+assert set(G.nodes()) == {'A', 'B', 'C', 'D', 'E', 'F'}
+
+assert G.number_of_nodes() == 6
+assert G.number_of_edges() == 5
+
+assert G['A']['B']['weight'] == 5
+assert G['A']['C']['weight'] == 3
+assert G['B']['C']['weight'] == 10
+assert G['C']['D']['weight'] == 1
+assert G['E']['F']['weight'] == 5
+
 
 
