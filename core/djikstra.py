fff962e50cdb2451aaea821be9395f7b9c2e9f50
Alana Andrade <alanandrade.vanessa@gmail.com>
2025-11-20 12:09:30 -0300

    djikstra implementation

--- core/djikstra.py
+++ core/djikstra.py
@@ -3,25 +3,14 @@ import heapq
 import networkx as nx
 
 
-def has_negative_weight(graph: nx.Graph) -> bool:
+def has_negative_weight(graph: nx.DiGraph) -> bool:
     for _, _, data in graph.edges(data=True):
         if data.get('weight', 1) < 0:
             return True
     return False
 
 
-# Retorna o peso total do grafo
-# Soma todos os pesos das arestas 
-def graph_weight(graph: nx.Graph) -> float:
-    return graph.size(weight="weight")
-
-# Retorna um dicionario com as arestas e seus pesos
-# No formato {(a, b): peso}
-def all_edges_sizes(graph: nx.Graph):
-    return nx.get_edge_attributes(graph, "weight")
-
-
-def djikstra(graph: nx.Graph, start: nx.Node, end: nx.Node):
+def djikstra(graph: nx.DiGraph, start: str, end: str):
     
     # validations that will be performed by validation.py
     if graph is None or start is None or end is None:
@@ -40,4 +29,50 @@ def djikstra(graph: nx.Graph, start: nx.Node, end: nx.Node):
     heapq.heappush(unvisited, (0, start))
 
     while unvisited:
-        curr_dist, curr_node = 
+        curr_dist, curr_node = heapq.heappop(unvisited)
+
+        if curr_dist > dist.get(curr_node, float('inf')):
+            continue
+        
+        if curr_node == end:
+            path = []
+            node = end
+            while node is not None:
+                path.append(node)
+                node = pred[node]
+            path.reverse()
+            return curr_dist, path
+        
+        for neighbor in graph.neighbors(curr_node):
+            edge = graph.get_edge_data(curr_node, neighbor)
+            edge_weight = edge.get("weight", 1)
+            new_dist = curr_dist + edge_weight
+
+            if new_dist < dist.get(neighbor, float('inf')):
+                dist[neighbor] = new_dist
+                pred[neighbor] = curr_node
+                heapq.heappush(unvisited, (new_dist, neighbor))
+    
+    return float('inf'), None
+
+
+G = nx.DiGraph()
+G.add_edge("A", "B", weight=4)
+G.add_edge("A", "C", weight=2)
+G.add_edge("C", "B", weight=1)
+G.add_edge("B", "D", weight=5)
+
+# start = end
+dist, path = djikstra(G, "A", "A")
+assert dist == 0
+assert path == ["A"]
+
+# A → C → B < A → B
+dist, path = djikstra(G, "A", "B")
+assert dist == 3
+assert path == ["A", "C", "B"]
+
+# no path of B to C
+dist, path = djikstra(G, "B", "C")
+assert dist == float("inf")
+assert path == None
