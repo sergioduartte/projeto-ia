ed0b64245ce8ce6901d90bd896f0441838f2e5a4
lorenacarvalho <lorena.nascimento.carvalho@ccc.ufcg.edu.br>
2025-11-21 16:39:52 -0300

    melhoria nos testes estaticos para o core/dijkstra

--- core/dijkstra.py
+++ core/dijkstra.py
@@ -1,19 +1,23 @@
+"""
+Implementação do algoritmo de Dijkstra para encontrar o caminho mais curto em um grafo direcionado.
+"""
 #coding: utf-8
 import heapq
 import networkx as nx
-
 from util import validator
 
-
-def dijkstra(graph: nx.DiGraph, start: str, end: str) -> tuple[float|None, list[str]]:
-
-    validator.validate_objects(graph, start, end)
+def dijkstra(digraph: nx.DiGraph, start: str, end: str) -> tuple[float|None, list[str]]:
+    """
+    Encontra o caminho mais curto entre os nós start e end
+    Retorna uma tupla com a distância total e a lista de nós no caminho.
+    Se não houver caminho, retorna (float('inf'), None).
+    """
+    validator.validate_objects(digraph, start, end)
     if start == end:
         return 0, [start]
-    
+
     pred = {start: None} # keep predecessors nodes
     dist = {start: 0} # keep distance of node to start
-
     unvisited = []
     heapq.heappush(unvisited, (0, start))
 
@@ -22,7 +26,6 @@ def dijkstra(graph: nx.DiGraph, start: str, end: str) -> tuple[float|None, list[
 
         if curr_dist > dist.get(curr_node, float('inf')):
             continue
-        
         if curr_node == end:
             path = []
             node = end
@@ -31,9 +34,9 @@ def dijkstra(graph: nx.DiGraph, start: str, end: str) -> tuple[float|None, list[
                 node = pred[node]
             path.reverse()
             return curr_dist, path
-        
-        for neighbor in graph.neighbors(curr_node):
-            edge = graph.get_edge_data(curr_node, neighbor)
+
+        for neighbor in digraph.neighbors(curr_node):
+            edge = digraph.get_edge_data(curr_node, neighbor)
             edge_weight = edge.get("weight", 1)
             new_dist = curr_dist + edge_weight
 
@@ -41,27 +44,25 @@ def dijkstra(graph: nx.DiGraph, start: str, end: str) -> tuple[float|None, list[
                 dist[neighbor] = new_dist
                 pred[neighbor] = curr_node
                 heapq.heappush(unvisited, (new_dist, neighbor))
-    
     return float('inf'), None # no path of start to end
 
-
-G = nx.DiGraph()
-G.add_edge("A", "B", weight=4)
-G.add_edge("A", "C", weight=2)
-G.add_edge("C", "B", weight=1)
-G.add_edge("B", "D", weight=5)
+graph = nx.DiGraph()
+graph.add_edge("A", "B", weight=4)
+graph.add_edge("A", "C", weight=2)
+graph.add_edge("C", "B", weight=1)
+graph.add_edge("B", "D", weight=5)
 
 # start = end
-dist, path = dijkstra(G, "A", "A")
+dist, path = dijkstra(graph, "A", "A")
 assert dist == 0
 assert path == ["A"]
 
 # A → C → B < A → B
-dist, path = dijkstra(G, "A", "B")
+dist, path = dijkstra(graph, "A", "B")
 assert dist == 3
 assert path == ["A", "C", "B"]
 
 # no path of B to C
-dist, path = dijkstra(G, "B", "C")
+dist, path = dijkstra(graph, "B", "C")
 assert dist == float("inf")
-assert path == None
+assert path is None
